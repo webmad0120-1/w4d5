de9c33f6b2848a4b68ca8a606901b0ab6d739872
Dani Vicario <danivicario@dvicario.es>
2020-02-07 10:38:20 +0100

    added

---
+++ livecoding/server-bcrypt.js
@@ -0,0 +1,13 @@
+const bcrypt = require("bcrypt");
+
+const saltRounds = 5;
+
+const plainPassword = "momo";
+const salt = bcrypt.genSaltSync(saltRounds);
+const hash = bcrypt.hashSync(plainPassword, salt);
+
+console.log(`plainPassword: ${plainPassword} 
+-- salt: ${salt}
+-- hash: ${hash}`);
+
+console.log(bcrypt.compareSync(plainPassword, `${hash}`));
